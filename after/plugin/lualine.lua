d6de37d368a09aab4b0ab1349a5c44c18bb6b6fa
Jakob Sachs <jakobsachs1999@gmail.com>
2023-12-04 17:29:24 +0100

    Changed setup to work with custom typing-metrics.nvim plugin

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,1 +1,19 @@
-require('lualine').setup()
+function wpm_raw()
+    return require("typing-metrics").get_statusline_raw()
+end
+
+function wpm()
+    return require("typing-metrics").get_statusline()
+end
+
+require('lualine').setup({
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { wpm, wpm_raw },
+        lualine_z = { 'location' }
+
+    },
+})
--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
+vim.opt.runtimepath:prepend("/Users/jakobsachs/git/typing-metrics.lua")
+
+require("typing-metrics").setup({})
+
 require("user.remap")
 require("user.set")
 require("user.plugins")
-
-
